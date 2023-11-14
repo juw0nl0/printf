7d34c40a7719b18df33fc316b08681dd4aac684c
Mahlet <mahlett271@gmail.com>
2023-11-14 11:35:26 -0800

    Revert "_printf.c"
    This reverts commit 5546b08415e9264773b56478f14ac209c6e6274c.

--- _printf.c
+++ _printf.c
@@ -1,46 +1,62 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - a function that produces output according to a format.
+ * @format: a formatted input to the function.
+ *
+ * Return: the number of characters printed
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert p[] = {
-		{"%s", print_s}, {"%c", print_c},
-		{"%%", print_37},
-		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
-		{"%R", print_rot13}, {"%b", print_bin},
-		{"%u", print_unsigned},
-		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
-		{"%S", print_exc_string}, {"%p", print_pointer}
-	};
+	int count = 0;
+	va_list args_list;
 
-	va_list args;
-	int i = 0, j, length = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	if (format == NULL)
 	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
-			{
-				length += p[j].function(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		length++;
-		i++;
+		return (-1);
 	}
-	va_end(args);
-	return (length);
+	va_start(args_list, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				count++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args_list, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args_list, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+				{
+					str_len++;
+				}
+				write(1, str, str_len);
+				count += str_len;
+			}
+			else if (*format == '\0')
+			{
+				break;
+			}
+		}
+		format++;
+	}
+	va_end(args_list);
+	return (count);
 }
