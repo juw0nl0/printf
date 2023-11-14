d8813ca7dc72f013567a387b3686dbda0284ecc0
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-14 09:05:40 -0800

    correction 2

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * print_char - Helper function to print a character
--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H_INCLUDED
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
