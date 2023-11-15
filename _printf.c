d963b2356acddc22802af746d3b101fcdac62193
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-15 06:59:43 -0800

    betty correction

--- _printf.c
+++ _printf.c
@@ -12,48 +12,34 @@ int _printf(const char *format, ...)
 	va_list args_list;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 	va_start(args_list, format);
 	while (*format)
 	{
 		if (*format != '%')
-		{
 			write(1, format, 1);
 			count++;
-		}
 		else
 		{
 			format++;
 			if (*format == '%')
-			{
 				write(1, format, 1);
 				count++;
-			}
 			else if (*format == 'c')
-			{
 				char c = va_arg(args_list, int);
 
 				write(1, &c, 1);
 				count++;
-			}
 			else if (*format == 's')
-			{
 				char *str = va_arg(args_list, char*);
 				int str_len = 0;
 
 				while (str[str_len] != '\0')
-				{
 					str_len++;
-				}
 				write(1, str, str_len);
 				count += str_len;
-			}
 			else if (*format == '\0')
-			{
 				break;
-			}
 		}
 		format++;
 	}
